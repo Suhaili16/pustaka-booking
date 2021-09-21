06e370f3361ea2abdeb05248bfb8582563d3aadb
Suhaili16 <suhail12182338@nusamandiri.ac.id>
2021-09-21 11:13:05 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> PERKENALKAN </h1>";
+        echo "Nama saya Suhaili hobi saya rebahan";
+
+
+    }
+
+
+}
